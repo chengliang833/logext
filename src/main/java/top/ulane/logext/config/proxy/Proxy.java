36528ecdbb5a649f1f5e8e6d7c14167b504359cb
程亮 <cl@ulane.top>
2021-05-22 14:54:02 +0800

    单独配置自定义代理

---
+++ src/main/java/top/ulane/logext/config/proxy/Proxy.java
@@ -0,0 +1,10 @@
+package top.ulane.logext.config.proxy;
+
+import org.springframework.stereotype.Component;
+
+import top.ulane.logext.config.LogAspectExt;
+
+@Component
+public class Proxy extends LogAspectExt {
+
+}
